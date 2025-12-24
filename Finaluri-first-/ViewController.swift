476b1b945a9d8fb42fc4523839d49310d8e02025
nikagiorgadze453 <nikagiorgadze06@gmail.com>
2025-12-24 23:13:34 +0400

    Initial Commit

---
+++ Finaluri-first-/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finaluri-first-
+//
+//  Created by Linda Mamardashvili on 24.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
